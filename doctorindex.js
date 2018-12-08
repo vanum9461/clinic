fa05dc22fecd4bcf21f70cc938fc20f16960448c
vanum9461 <43624853+vanum9461@users.noreply.github.com>
2018-12-08 07:19:17 +0530

    Add files via upload

---
+++ doctorindex.js
@@ -0,0 +1,66 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      //document.getElementById("user_para").innerHTML = "You are logged in with : " + email_id;
+      window.open("http://localhost:8080/");
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+
+}
+
+
+function signup()
+{
+email = document.getElementById("email_field").value;
+pass = document.getElementById("password_field").value;
+// Sign in with email and pass.
+  // [START createwithemail]
+  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
